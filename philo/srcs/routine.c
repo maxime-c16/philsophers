4fcf4566b81a08de81c7520ebf22e8f45d080638
nino <nino.faivre@netc.fr>
2022-08-12 08:55:56 +0200

    lol

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -6,40 +6,22 @@
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 15:44:44 by mcauchy           #+#    #+#             */
-/*   Updated: 2022/08/11 19:33:59 by mcauchy          ###   ########.fr       */
+/*   Updated: 2022/08/12 08:55:10 by nfaivre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-static pthread_mutex_t	*lol(t_philo *philo, int i)
-{
-	if (!(philo->id % 2))
-	{
-		if (!i)
-			return (philo->right_fork);
-		else
-			return (philo->left_fork);
-	}
-	else
-	{
-		if (!i)
-			return (philo->left_fork);
-		else
-			return (philo->right_fork);
-	}
-}
-
 static void	take_fork(t_philo *philo)
 {
-	pthread_mutex_lock(lol(philo, 0));
+	pthread_mutex_lock((pthread_mutex_t *)((!(philo->id % 2) * (long long int)philo->right_fork) + ((philo->id %2 != 0) * (long long int)philo->left_fork)));
 	mutex_message("has taken a fork", philo);
 	if (is_dead(philo))
 	{
-		pthread_mutex_unlock(lol(philo, 0));
+		pthread_mutex_unlock((pthread_mutex_t *)((!(philo->id % 2) * (long long int)philo->right_fork) + ((philo->id %2 != 0) * (long long int)philo->left_fork)));
 		return ;
 	}
-	pthread_mutex_lock(lol(philo, 1));
+	pthread_mutex_lock((pthread_mutex_t *)(((philo->id % 2 != 0) * (long long int)philo->right_fork) + (!(philo->id %2) * (long long int)philo->left_fork)));
 	mutex_message("has taken a fork", philo);
 	philo_eat(philo);
 	pthread_mutex_unlock(philo->right_fork);
